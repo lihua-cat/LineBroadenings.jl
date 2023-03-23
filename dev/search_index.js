8a20ba1a34defd1ccf7a836276a69846e2aaaa10
Documenter.jl <documenter@juliadocs.github.io>
2023-03-23 06:39:59 +0000

    build based on 7828800

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LineBroadenings","category":"page"},{"location":"#LineBroadenings","page":"Home","title":"LineBroadenings","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LineBroadenings.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LineBroadenings]","category":"page"}]
+}
